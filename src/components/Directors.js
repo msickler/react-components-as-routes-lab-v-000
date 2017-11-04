3a605e79fc3e6c4b7047586a8b5519f314e8e060
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 17:46:17 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,7 +5,7 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
-      {directors.map(director => 
+      {directors.map(director =>
         <div>
         <h3>{director.name}</h3>
         <ul>{director.movies.map(movie => <li>{movie}</li>)}</ul>
