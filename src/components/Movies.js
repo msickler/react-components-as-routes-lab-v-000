b6313fdefe5fe11efb321e0300ea4cca07bfaf14
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 17:43:30 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,7 +6,7 @@ const Movies = () => {
     <div>
       <h1>Movies Page</h1>
        {movies.map(movie => (
-         <div> 
+         <div>
           <h3>{movie.title}</h3>
           <p>{movie.time}</p>
           <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
